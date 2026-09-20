e66eeede23981204ce27f45bfd8d08eae26be988
vishalcoder0912 <vishal.kumar.1304200504@gmail.com>
2026-09-20 11:27:24 +0530

    docs: add project header comment to frontend/src/pages/ActivityLogs.tsx

--- frontend/src/pages/ActivityLogs.tsx
+++ frontend/src/pages/ActivityLogs.tsx
@@ -1,3 +1,4 @@
+// hello this is vishal project
 import { useMemo, useState } from 'react';
 import { PageHeader, FilterSelect, Toolbar, Panel, Badge, EmptyState } from '../components/ui';
 import { useCollection, fmtDate, fmtTime, daysSince } from '../lib/hooks';
