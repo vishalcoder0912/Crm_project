07f6157a1ca94b7ad055dac2b327ba167b928d63
vishalcoder0912 <vishal.kumar.1304200504@gmail.com>
2026-09-20 11:27:22 +0530

    docs: add project header comment to frontend/src/components/CommandPalette.tsx

--- frontend/src/components/CommandPalette.tsx
+++ frontend/src/components/CommandPalette.tsx
@@ -1,3 +1,4 @@
+// hello this is vishal project
 import { useEffect, useMemo, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { api, isDemo } from '../lib/api';
